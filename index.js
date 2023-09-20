8b70bd81324ccbadad31d78f968c9e34dbf294a8
Dawn Madden <coachdawnmadden@gmail.com>
2023-09-20 19:27:57 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, string){
+    let matchingDrivers = [];
+    for (let i = 0; i < drivers.length; i++){
+        let lowercaseDriverName = drivers[i].toLowerCase();
+        if (lowercaseDriverName.includes(string.toLowerCase())){
+            matchingDrivers.push(drivers[i]);
+        }
+    }
+    return matchingDrivers;
+}
+function fuzzyMatch (drivers, string){
+    let fuzzyDrivers = [];
+    for (let i = 0; i < drivers.length; i++)
+    if (drivers[i].startsWith(string)){
+        fuzzyDrivers.push(drivers[i]);
+    }
+    return fuzzyDrivers;
+}
+function matchName(drivers, string){
+    const nameObject = [];
+    for (const driver of drivers){
+        if (driver.name ===string){
+            nameObject.push(driver);
+        }
+    }
+    return nameObject;
+}
